c4a52201a12f459c62d751c635a5f3de5d4e9c4b
Dmitriy Oliferov <DrEfrit@yandex.ru>
2022-04-19 12:27:17 +0300

    Initial commit

---
+++ app/src/main/java/com/oliferov/agriculturalstore/GoodsListActivity.kt
@@ -0,0 +1,11 @@
+package com.oliferov.agriculturalstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoodsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goods_list)
+    }
+}
